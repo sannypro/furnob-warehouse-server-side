eb63085ccf3f6a32733ff1fccd4bd32034e09c2d
sannypro <sannymr79@gmail.com>
2022-04-29 21:08:53 +0600

    single product load api done

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 const query = require('express/lib/middleware/query');
 const res = require('express/lib/response');
@@ -15,14 +15,21 @@ app.use(cors())
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.ysjbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
+
     try {
+        await client.connect();
+        const inventoryCollection = client.db('furnob-furniture-warehouse').collection("inventory");
         app.get('/', async (req, res) => {
-            await client.connect();
-            const inventoryCollection = client.db('furnob-furniture-warehouse').collection("inventory");
             const query = {};
             const cursor = inventoryCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
+        })
+        app.get('/inventory/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const product = await inventoryCollection.findOne(query);
+            res.send(product);
         })
     }
     finally {
