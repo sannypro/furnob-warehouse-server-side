0d032533958d2cc28e1021e003a6b39d3dd376fa
sannypro <sannymr79@gmail.com>
2022-04-29 11:28:03 +0600

    connected with mongodb

--- index.js
+++ index.js
@@ -1,18 +1,35 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express');
+const query = require('express/lib/middleware/query');
+const res = require('express/lib/response');
 require('dotenv').config()
 const app = express();
+const cors = require('cors')
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(express.json())
+app.use(express.json());
 app.use(cors())
 
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.ysjbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+async function run() {
+    try {
+        app.get('/', async (req, res) => {
+            await client.connect();
+            const inventoryCollection = client.db('furnob-furniture-warehouse').collection("inventory");
+            const query = {};
+            const cursor = inventoryCollection.find(query);
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+    }
+    finally {
 
-
+    }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
     console.log(` listening on port ${port}`)
