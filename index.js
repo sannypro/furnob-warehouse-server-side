6886655723c487e9be8271d7edfcabe729f9079c
sannypro <sannymr79@gmail.com>
2022-05-01 14:13:49 +0600

    jwt token verification system implemented

--- index.js
+++ index.js
@@ -5,12 +5,28 @@ const res = require('express/lib/response');
 require('dotenv').config()
 const app = express();
 const cors = require('cors')
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
 app.use(cors())
+function verifyToken(req, res, next) {
+    const authHeader = req.headers.authorization;
 
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' })
+    }
+    const token = authHeader?.split(' ')[1]
+    jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Forbbiden' })
+        }
+        req.decoded = decoded;
+
+    })
+    next()
+}
 
 const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.ysjbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -24,6 +40,12 @@ async function run() {
             const cursor = inventoryCollection.find(query);
             const result = await cursor.toArray();
             res.send(result)
+        })
+        app.post('/login', (req, res) => {
+            const email = req.body
+
+            const accessToken = jwt.sign(email, process.env.ACCESS_TOKEN, { expiresIn: '1d' });
+            res.send(accessToken)
         })
         app.get('/inventory/:id', async (req, res) => {
             const id = req.params.id;
@@ -42,13 +64,23 @@ async function run() {
             const result = await inventoryCollection.insertOne(doc);
             res.send(result)
         })
-        app.get('/my-items', async (req, res) => {
-            const email = req.query.email;
-            const query = { email: email }
-            const cursor = inventoryCollection.find(query);
-            const result = await cursor.toArray()
+        app.get('/my-items', verifyToken, async (req, res) => {
+            const decodedEmail = req.decoded?.email
 
-            res.send(result)
+            const email = req.query.email;
+
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = inventoryCollection.find(query);
+                const result = await cursor.toArray();
+                res.send(result)
+            }
+            else {
+                res.status(403).send({ message: 'Forbbiden' })
+            }
+
+
+
 
         })
         app.put('/inventory/:id', async (req, res) => {
