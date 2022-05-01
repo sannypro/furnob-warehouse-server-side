662b9b9db5e9b1e9774e28a903cbc40bccd553a9
sannypro <sannymr79@gmail.com>
2022-05-01 17:13:10 +0600

    heroku error fixed

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function verifyToken(req, res, next) {
     next()
 }
 
-const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@cluster0.ysjbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.S3_BUCKET}:${process.env.SECRET_KEY}@cluster0.ysjbw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 async function run() {
 
